26fb6d24b7131382f59d58b71bd841ca4fa0bbd3
Naeun-K <nek7148@gmail.com>
2026-06-11 17:32:19 +0900

    Feat: Solve no.2 of homeworks

--- 2026-06-11/2026-06-11.js
+++ 2026-06-11/2026-06-11.js
@@ -1,6 +1,6 @@
 const scores = [85, 92, 58, 74, 66, 100, 43];
 
-//  1번 등급구하기
+// 문제 1. 등급구하기
 console.log("============ 1번 ==============");
 function getGrade(score) {
   if (score >= 90) {
@@ -15,8 +15,19 @@ function getGrade(score) {
     return "F";
   }
 }
-
 // 문제1. 등급구하기 결과 예시
 console.log(getGrade(92)); // A
 console.log(getGrade(74)); // C
 console.log(getGrade(43)); // F
+
+// 문제 2. 합격 여부 구하기
+console.log("============ 2번 ==============");
+function isPassed(score) {
+  if (score >= 60) {
+    return true;
+  }
+  return false;
+}
+// 문제2. 합격 여부 구하기 결과 예시
+console.log(isPassed(85)); // true
+console.log(isPassed(43)); // false
